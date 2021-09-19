03557474303f7bc3e48dc70ddaef9009e0762e05
DiKiALiF <diki_171@smkassalaambandung.sch.id>
2021-09-19 15:16:54 +0700

    latihan turunan

--- oop/latihanturunan.php
+++ oop/latihanturunan.php
@@ -18,7 +18,7 @@
   <center><img src="download.png" /><br><br>
 <h2>PENGGAJIHAN<br>GURU/KARYAWAN YAYASAN ASSALAAM</h2></center><br/><br/>
 
-<form method="POST" action="latihanturunan2.php">
+<form method="POST" action="">
 
 <div class="form-group row">
     <label for="inputEmail3" class="col-sm-2 col-form-label">No</label>
@@ -67,7 +67,7 @@
     <label for="inputEmail3" class="col-sm-2 col-form-label">Status Kerja</label>
     <div class="col-sm-8">
     <select class="form-control" id="exampleFormControlSelect1" name="status_kerja">
-<option>Pergawai Tetap</option>
+<option>Pegawai Tetap</option>
 <option>Pegawai Konrak</option>
 </select>
     </div></div>
@@ -95,7 +95,98 @@
     </div></div>
     <br><center><input type="submit" name="simpan" class="btn btn-primary" value="Submit"></center><br>
 
+    <?php
 
+if(isset($_POST['simpan'])){
+    $no = $_POST['no'];
+    $nama = $_POST['nama'];
+    $unit = $_POST['unit'];
+    $tanggal = $_POST['tanggal'];
+    $jabatan = $_POST['jabatan'];
+    $lamker = $_POST['lama_kerja'];
+    $staker = $_POST['status_kerja'];
+    $bpjs = $_POST['bpjs'];
+    $pinjaman = $_POST['pinjaman'];
+    $tabungan = $_POST['tabungan'];
+    $lainnya = $_POST['lainnya'];
+
+class penggajihan {
+    public $total_gaji;
+    public $total_pengeluaran;
+function pnggjhn($no, $nama, $unit, $tanggal) {
+echo "<table align=center>";
+echo "<tr><td>No</td><td>:</td><td>{$no}</td></tr>";
+echo "<tr><td>Nama</td><td>:</td><td>{$nama}</td></tr>";
+echo "<tr><td>Unit</td><td>:</td><td>{$unit}</td></tr>";
+echo "<tr><td>Tanggal Gaji</td><td>:</td><td>{$tanggal}</td></tr>";
+}
+}
+
+class gaji extends penggajihan {
+function gj($jabatan, $lamker, $staker) {
+echo "<tr><td>&nbsp</td></tr>";
+echo "<tr><td><td><td><b><i>Gaji</i></b></td></td></td><tr>";
+echo "<tr><td>&nbsp</td></tr>";
+echo "<tr><td>Jabatan</td><td>:</td><td>{$jabatan}</td></tr>";
+if ($jabatan == "Kepala Sekolah"){
+    $gajih = 5000000;
+} elseif ($jabatan == "Guru"){
+    $gajih = 3000000;
+} elseif ($jabatan == "Karyawan"){
+    $gajih = 1500000;
+} else {
+    $gajih = 0;
+}
+echo "<tr><td>Gaji</td><td>:</td><td>Rp. {$gajih}</td></tr>";
+echo "<tr><td>Lama Kerja</td><td>:</td><td>{$lamker} Tahun</td></tr>";
+if ($lamker <= 5){
+    $bonus = 0;
+} elseif ($lamker > 5){
+    $bonus = 250000;
+} elseif ($lamker > 10){
+    $bonus = 500000;
+} 
+echo "<tr><td>Bonus Lama Kerja</td><td>:</td><td>Rp. {$bonus}</td></tr>";
+echo "<tr><td>Status Kerja</td><td>:</td><td>{$staker}</td></tr>";
+if ($staker == "Pegawai Tetap"){
+    $tnjngn = 500000;
+} elseif ($staker == "Pegawai Kontrak"){
+    $tnjngn = 0;
+}
+echo "<tr><td>Tunjangan Status Kerja</td><td>:</td><td>Rp. {$tnjngn}</td></tr>";
+$this->total_gaji = $gajih + $bonus + $tnjngn;
+echo "<tr><td><b>Total Gaji</td><td>:</td><td><b>Rp. " . $this->total_gaji . "</b>";
+}
+}
+
+class potongan extends gaji {
+function ptngn($bpjs, $pinjaman, $tabungan, $lainnya) {
+    echo "<tr><td>&nbsp</td></tr>";
+    echo "<tr><td><td><td><b><i>Potongan</i></b></td></td></td><tr>";
+    echo "<tr><td>&nbsp</td></tr>";
+    echo "<tr><td>BPJS</td><td>:</td><td>Rp. {$bpjs}</td></tr>";
+    echo "<tr><td>Pinjaman</td><td>:</td><td>Rp. {$pinjaman}</td></tr>";
+    echo "<tr><td>Tabungan</td><td>:</td><td>Rp. {$tabungan}</td></tr>";
+    echo "<tr><td>Lainnya</td><td>:</td><td>Rp. {$lainnya}</td></tr>";
+    $this->total_pengeluaran = $bpjs + $pinjaman + $tabungan + $lainnya;
+    echo "<tr><td><b>Total Potongan</td><td>:</td><td><b>Rp. " . $this->total_pengeluaran . "</b>";
+    $sisa = $this->total_gaji - $this->total_pengeluaran;
+    echo "<tr><td>&nbsp</td></tr>";
+    echo "<tr><td><b><i>Jumlah Yang Diterima <td>:</td><td> <b> <i> Rp. " . $sisa . "</i></b></td></td></tr>";
+}
+}
+
+}
+
+$a = new potongan;
+
+$a -> pnggjhn($no, $nama, $unit, $tanggal);
+
+$a -> gj($jabatan, $lamker, $staker);
+
+$a -> ptngn($bpjs, $pinjaman, $tabungan, $lainnya);
+
+?> 
     
 </div>
 </form>
